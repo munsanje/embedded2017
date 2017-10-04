6e27d4a2dad621c7e9b897c86127cffe1f8ddfc2
Amisi Kiarie <amisikiarie@gmail.com>
2017-10-04 10:46:18 +0200

    Configured ADC1 to read pots and update w/ DMA

--- src/uinput.c
+++ src/uinput.c
@@ -6,92 +6,97 @@
 #include "stm32f4xx_rcc.h"
 
 
-const uint16_t LEDS = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-const uint16_t LED[4] = {GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15};
-
-void setup_adc() {
-    // enable peripherals
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); // ADC interface clock
-	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN, ENABLE); // GPIO interface clock for ADC
-
-    // setup GPIO pins for ADC
-	GPIO_InitTypeDef GPIO_InitStructre;
-	GPIO_StructInit(&GPIO_InitStructre);
-	GPIO_InitStructre.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
-	GPIO_InitStructre.GPIO_Mode = GPIO_Mode_AN;
-	GPIO_InitStructre.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOC, &GPIO_InitStructre);
-
-    // configure ADC
-	ADC_DeInit();
-	ADC_InitTypeDef ADC_InitStructure;
-	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-	ADC_InitStructure.ADC_Resolution = ADC_Resolution_6b;
-	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
-	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-	ADC_InitStructure.ADC_NbrOfConversion = 2;
-	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
-	ADC_Init(ADC1, &ADC_InitStructure);
-
-    // select channels for conversion
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles);
-    
-    // enable conversion
-    ADC_ContinuousModeCmd(ADC1, ENABLE);
-    ADC_DMACmd(ADC1, ENABLE);
-    ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
-    /*ADC_Cmd(ADC1, ENABLE);*/
-}
-
-void setup_dma() {
-    RCC_AHB1PeriphResetCmd(RCC_AHB1Periph_DMA1, ENABLE);
-
-    DMA_InitTypeDef DMA_InitStruct;
-    DMA_StructInit(&DMA_InitStruct); // default values
-    DMA_InitStruct.DMA_DIR = DMA_DIR_MemoryToMemory;
-}
+#define ADC1_CHANNEL_PC0 10
+#define ADC1_CHANNEL_PC1 11
 
 
+// stores the values of the pots as updated by the ADC via DMA
+uint8_t Pot[2] = {0,0};
 
-uint16_t readADC1() {
-    ADC_SoftwareStartConv(ADC1);//Start the conversion
-    while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));//Processing the conversion
-    return ADC_GetConversionValue(ADC1); //Return the converted data
-}
+// configures ADC1 to read PC0, PC1 and fill potX, potY using DMA
+void setup_pots();
 
 void uinput_main(void* p) {
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-    GPIO_InitTypeDef gpio;
-    GPIO_StructInit(&gpio);
-    gpio.GPIO_Mode = GPIO_Mode_OUT;
-    gpio.GPIO_Pin = LEDS;
-    GPIO_Init(GPIOD, &gpio);
+    setup_pots();
 
+    // configure output pins for LEDs
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOB,&GPIO_InitStructure);
-
-    setup_adc();
-
-    uint16_t val = 0;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     uint8_t x,y = 0;
 
     for (;;) {
-        val = readADC1();
-        x = val/16;
-        y = 3 - x;
+        x = Pot[0] >> 4; // focus on 2 MSBs of 6-bit ADC output
+        y = Pot[1] >> 4;
 
         GPIOB->ODR = (x << 6) | (y << 4);
-        GPIO_ResetBits(GPIOD, LEDS);
-        GPIO_SetBits(GPIOD, LED[x]);
     }
     vTaskDelete(NULL);
 }
+
+void setup_pots() {
+    ADC_InitTypeDef       ADC_InitStruct;
+    ADC_CommonInitTypeDef ADC_CommonInitStruct;
+    DMA_InitTypeDef       DMA_InitStruct;
+    GPIO_InitTypeDef      GPIO_InitStruct;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2 | RCC_AHB1Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+
+    // configure PCO, PC1
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+    GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    // set DMA2 to two values from read from ADC1
+    DMA_InitStruct.DMA_Channel = DMA_Channel_0;
+    DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)((uintptr_t)&ADC1->DR);
+    DMA_InitStruct.DMA_Memory0BaseAddr = (uint32_t)((uintptr_t)&Pot);
+    DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralToMemory;
+    DMA_InitStruct.DMA_BufferSize = 2;
+    DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStruct.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStruct.DMA_Priority = DMA_Priority_High;
+    DMA_InitStruct.DMA_FIFOMode = DMA_FIFOMode_Disable;
+    DMA_InitStruct.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+    DMA_InitStruct.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+    DMA_InitStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+    DMA_Init(DMA2_Stream0, &DMA_InitStruct);
+    DMA_Cmd(DMA2_Stream0, ENABLE);
+
+    // set ADC1 to read PC0, PC1 and send to DMA1
+    ADC_CommonInitStruct.ADC_Mode = ADC_Mode_Independent;
+    ADC_CommonInitStruct.ADC_Prescaler = ADC_Prescaler_Div2;
+    ADC_CommonInitStruct.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+    ADC_CommonInitStruct.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+    ADC_CommonInit(&ADC_CommonInitStruct);
+
+    ADC_DeInit();
+    ADC_InitStruct.ADC_Resolution = ADC_Resolution_6b;
+    ADC_InitStruct.ADC_ScanConvMode = ENABLE;
+    ADC_InitStruct.ADC_ContinuousConvMode = ENABLE;
+    ADC_InitStruct.ADC_ExternalTrigConv = 0;
+    ADC_InitStruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+    ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitStruct.ADC_NbrOfConversion = 2;
+    ADC_Init(ADC1, &ADC_InitStruct);
+
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles); // PC0
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 2, ADC_SampleTime_144Cycles); // PC1
+
+    ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
+    ADC_DMACmd(ADC1, ENABLE);
+    ADC_Cmd(ADC1, ENABLE);
+
+    ADC_SoftwareStartConv(ADC1);
+}
+
