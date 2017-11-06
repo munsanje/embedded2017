0af00093451fde9bab425b00d3dabb64e85e9a37
Amisi Kiarie <amisikiarie@gmail.com>
2017-11-06 23:06:53 +0200

    more compile

--- src/card.c
+++ src/card.c
@@ -9,17 +9,33 @@
 #include "sound.h"
 #include "wav.h"
 
-static FATFS FatFs;
-static FIL fp;
-
 void card_main(void* p) {
+	FATFS FatFs;
+	FIL fp;
+	int count =0;
+	SystemInit();
 	FRESULT res = init_sd(&FatFs);  // initialize SD card
-	const char* filename = "mA.wav";
-	uint32_t buffer_size = get_wav_size(&fp, filename);
-	uint16_t buffer[buffer_size];
-    res = read_wav_file(&fp, filename, buffer, buffer_size);  // load in PCM data
-	if(res == FR_OK) {
-		playBuffer(buffer, buffer_size);
+	const char* filename = "pG.wav"; //mA.wav";
+	const char* files[] = {"pG.wav","mCh.wav"};
+	uint16_t bufferP[11025];
+	for(int z=0;z<11025;z++){
+		bufferP[z]=0;
+	}
+	uint16_t buffer[11025];
+	for(int i=0;i<2;i++){
+		res = open_file(&fp, files[i], FA_READ | FA_OPEN_ALWAYS);
+			int k = 0; //
+		res = read_wav_file(&fp, files[i], buffer, 11025);
+		for(int k=0;k<11025;k++){
+			bufferP[k]=bufferP[k]+buffer[k];
+		}
+
 	}
 	deinit_sd();  // de-initialize SD card
+
+	playBuffer(bufferP, 11025);
+
+    for (;;) {
+    }
+    vTaskDelete(NULL);
 }
